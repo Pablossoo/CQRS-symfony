28cf01c5b549bfd9ca9563b8db1afc84c5e9fcf6
Paweł <pablos.lukowski@wp.pl>
2019-06-13 20:42:33 +0200

    rebuild construct creating users

---
+++ src/domain/event/UserCreated.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pawel
+ * Date: 07.06.2019
+ * Time: 00:12
+ */
+
+namespace App\domain\event;
+
+
+class UserCreated
+{
+
+}
---
+++ src/domain/listener/UserCreatedListener.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pawel
+ * Date: 07.06.2019
+ * Time: 00:17
+ */
+
+namespace App\domain\listener;
+
+
+class UserCreatedListener
+{
+
+}
