48a8ef899804d34b8941f77dbdc37be10bdfc958
Paweł <pablos.lukowski@wp.pl>
2019-06-19 17:55:14 +0200

    return last adding email

---
+++ src/application/query/user/UserQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\application\query\user;
+
+
+interface UserQuery
+{
+
+}
---
+++ src/infrastructure/doctrine/repository/UserViewDbal.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\infrastructure\doctrine\repository;
+
+
+/**
+ * @author Paweł Łukowski <pawell.lukowski@gmail.com>
+ */
+class UserViewDbal
+{
+
+}
